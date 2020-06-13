409dc058821a3e4e1288c48b627818efee3a2086
Makaa88 <maka6868@gmail.com>
2020-06-13 23:19:08 +0200

    react part

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginComponent from "./Components/LoginComponent";
+import RegisterComponent from "./Components/RegisterComponent";
+import ExpensesController from "./Components/ExpensesComponent";
+
+class App extends Component{
+
+      render() {
+        return(
+            <div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={LoginComponent}/>
+                        <Route exact path='/register' component={RegisterComponent}/>
+                        <Route path='/expenses' component={ExpensesController}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+
+        );
+      }
 }
 
 export default App;
